62b327c1e599c811b2e0f6bbe01ab233a0839491
lenonmerlo <lenontm@gmail.com>
2024-10-01 18:15:30 -0300

    Construindo o card de tarefas

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -6,12 +6,12 @@ const BarraLateral = () => (
     <S.Aside>
       <S.Campo type="text" placeholder="Pesquisar" />
       <S.Filtros>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard legenda="pendentes" contador={1} />
+        <FiltroCard legenda="concluídas" contador={2} />
+        <FiltroCard legenda="urgentes" contador={3} />
+        <FiltroCard legenda="importante" contador={4} />
+        <FiltroCard legenda="normal" contador={5} />
+        <FiltroCard legenda="todas" contador={10} ativo />
       </S.Filtros>
     </S.Aside>
   </aside>
