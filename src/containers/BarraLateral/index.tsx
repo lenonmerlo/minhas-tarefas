b2d3598dbc8d4a2e14c3ab2511dc7ad6628329c0
lenonmerlo <lenontm@gmail.com>
2024-10-01 16:34:22 -0300

    Construindo o componente de filtros

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,20 @@
-const BarraLateral = () => <aside> barra lateral</aside>
+import FiltroCard from '../../componentes/FiltroCard'
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <aside>
+    <S.Aside>
+      <S.Campo type="text" placeholder="Pesquisar" />
+      <S.Filtros>
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard />
+        <FiltroCard ativo />
+      </S.Filtros>
+    </S.Aside>
+  </aside>
+)
 
 export default BarraLateral
