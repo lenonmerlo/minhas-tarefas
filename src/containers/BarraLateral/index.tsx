a94c908ea19d89a0f852d37e5404c999d7dcafd2
lenonmerlo <lenontm@gmail.com>
2024-10-02 18:46:27 -0300

    Implementando as funcionalidades dos cards de busca.

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,20 +1,54 @@
+import { useDispatch, useSelector } from 'react-redux'
 import FiltroCard from '../../componentes/FiltroCard'
 import * as S from './styles'
+import { RootReducer } from '../../store'
+import { alteraTermo } from '../../store/reducers/filtro'
+import * as enums from '../../util/enums/Tarefa'
 
-const BarraLateral = () => (
-  <aside>
-    <S.Aside>
-      <S.Campo type="text" placeholder="Pesquisar" />
-      <S.Filtros>
-        <FiltroCard legenda="pendentes" contador={1} />
-        <FiltroCard legenda="concluídas" contador={2} />
-        <FiltroCard legenda="urgentes" contador={3} />
-        <FiltroCard legenda="importante" contador={4} />
-        <FiltroCard legenda="normal" contador={5} />
-        <FiltroCard legenda="todas" contador={10} ativo />
-      </S.Filtros>
-    </S.Aside>
-  </aside>
-)
+const BarraLateral = () => {
+  const dispatch = useDispatch()
+  const { termo } = useSelector((state: RootReducer) => state.filtro)
+
+  return (
+    <aside>
+      <S.Aside>
+        <S.Campo
+          type="text"
+          placeholder="Pesquisar"
+          value={termo}
+          onChange={(evento) => dispatch(alteraTermo(evento.target.value))}
+        />
+        <S.Filtros>
+          <FiltroCard
+            valor={enums.Status.PENDENTE}
+            criterio="status"
+            legenda="pendentes"
+          />
+          <FiltroCard
+            valor={enums.Status.CONCLUIDA}
+            criterio="status"
+            legenda="concluídas"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.URGENTE}
+            criterio="prioridade"
+            legenda="urgentes"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.IMPORTANTE}
+            criterio="prioridade"
+            legenda="importante"
+          />
+          <FiltroCard
+            valor={enums.Prioridade.NORMAL}
+            criterio="prioridade"
+            legenda="normal"
+          />
+          <FiltroCard criterio="todas" legenda="todas" />
+        </S.Filtros>
+      </S.Aside>
+    </aside>
+  )
+}
 
 export default BarraLateral
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import tarefasReducer from './reducers/tarefas'
+import filtlroReducer from './reducers/filtro'
 
 const store = configureStore({
-  reducer: { tarefas: tarefasReducer }
+  reducer: { tarefas: tarefasReducer, filtro: filtlroReducer }
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
